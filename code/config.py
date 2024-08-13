a01e981bb37dd6a2abb9f329139a973f09b17637
wwzll123 <102224941+wwzll123@users.noreply.github.com>
2024-08-13 15:49:37 +0800

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,5 +1,5 @@
-HHblits=r'/home/zengww/software/hhsuite/bin/hhblits'
-HHBLITS_DB=r'/home/zengww/lib/uniclust30_2018_08/uniclust30_2018_08'
-model_dir=r'/home/zengww/SCI/GeSite/exp_res/model'
-PDB_dir=r'/home/zengww/SCI/GeSite/RNA-PDB-Dataset'
+HHblits=r'/home/anonymity/software/hhsuite/bin/hhblits'
+HHBLITS_DB=r'/home/anonymity/lib/uniclust30_2018_08/uniclust30_2018_08'
+model_dir=r'/home/anonymity/SCI/GeSite/exp_res/model'
+PDB_dir=r'/home/anonymity/SCI/GeSite/RNA-PDB-Dataset'
 threshold=0.5
